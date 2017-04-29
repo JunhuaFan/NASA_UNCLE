da5b3ec7c422962df07fcf7f069fa1043c07565b
BPC\bll <a1298tsex@hotmail.com>
2017-04-29 12:43:25 +0800

    init

---
+++ nasauncle/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(nasauncle.Startup))]
+namespace nasauncle
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
